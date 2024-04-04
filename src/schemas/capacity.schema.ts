e2a9874e268a5489edccd0ba5d788345eb42ffb1
Paul Vega <paulvega@Pauls-MacBook-Air.local>
2024-04-03 22:51:41 -0500

    first changes

---
+++ src/schemas/capacity.schema.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type TaskDocument = HydratedDocument<Capacity>;
+
+@Schema({
+  timestamps: true,
+})
+export class Capacity {
+  @Prop({ unique: true, trim: true, required: true })
+  description: string;
+}
+
+export const CapacitySchema = SchemaFactory.createForClass(Capacity);
